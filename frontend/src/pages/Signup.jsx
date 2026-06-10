22481eb49dc119aab28f8507d7591aced95a35a1
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-06-10 15:31:26 +0530

    done

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -114,6 +114,12 @@ const Signup = () => {
 										</svg>
 										<span className="text-sm">Facebook</span>
 									</button>
+									<button type="button" onClick={() => handleSocialLogin('instagram')} className="flex-1 flex items-center justify-center gap-3 px-3 py-2 rounded-md bg-gradient-to-br from-[#f58529] via-[#dd2a7b] to-[#8134af] text-white hover:opacity-95">
+										<svg className="w-5 h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+											<path fill="#ffffff" d="M7 2h10a5 5 0 015 5v10a5 5 0 01-5 5H7a5 5 0 01-5-5V7a5 5 0 015-5zm5 6.5a4.5 4.5 0 100 9 4.5 4.5 0 000-9zm5.5-3.5a1 1 0 11-2 0 1 1 0 012 0z" />
+										</svg>
+										<span className="text-sm">Instagram</span>
+									</button>
 								</div>
 							</div>
 						</form>
@@ -162,6 +168,12 @@ const Signup = () => {
 											<path fill="#ffffff" d="M22.675 0h-21.35C.597 0 0 .597 0 1.333v21.333C0 23.403.597 24 1.325 24H12.82v-9.294H9.692V11.01h3.128V8.413c0-3.1 1.894-4.788 4.66-4.788 1.325 0 2.463.099 2.793.143v3.24l-1.918.001c-1.504 0-1.796.716-1.796 1.765v2.315h3.587l-.467 3.696h-3.12V24h6.116C23.403 24 24 23.403 24 22.667V1.333C24 .597 23.403 0 22.675 0z"/>
 										</svg>
 										<span className="text-sm">Facebook</span>
+									</button>
+									<button type="button" onClick={() => handleSocialLogin('instagram')} className="flex-1 flex items-center justify-center gap-3 px-3 py-2 rounded-md bg-gradient-to-br from-[#f58529] via-[#dd2a7b] to-[#8134af] text-white hover:opacity-95">
+										<svg className="w-5 h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+											<path fill="#ffffff" d="M7 2h10a5 5 0 015 5v10a5 5 0 01-5 5H7a5 5 0 01-5-5V7a5 5 0 015-5zm5 6.5a4.5 4.5 0 100 9 4.5 4.5 0 000-9zm5.5-3.5a1 1 0 11-2 0 1 1 0 012 0z" />
+										</svg>
+										<span className="text-sm">Instagram</span>
 									</button>
 								</div>
 							</div>
