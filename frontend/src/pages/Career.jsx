027a96fe34a121193af4dba84e31a756019d305f
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-06-20 11:45:49 +0530

    career page evaluate

--- frontend/src/pages/Career.jsx
+++ frontend/src/pages/Career.jsx
@@ -229,7 +229,7 @@ const Career = () => {
 
       {/* Current Job Openings Section */}
       <div className="py-16 pt-8 bg-gray-50">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-3">
             <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-1">Current Job Openings</h2>
             
